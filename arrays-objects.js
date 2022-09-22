e40d8c40684870d4c0e2bfe7efa4f0852966e606
EvilGenius <93214149+EvilGenius13@users.noreply.github.com>
2022-09-21 21:19:12 -0400

    Add files via upload

---
+++ arrays-objects.js
@@ -0,0 +1,13 @@
+//Arrays, Objects, Nested Arrays
+const pokeBalls = [Pokeball, Greatball, Ultraball, Masterball]
+const itemPrice = [15, 30, 45, 60]
+const shoppingcart = []
+const pokemon =  [{
+    name: Charmander, type: fire, pokedexnum: 004},
+{   name: Bulbasaur, type: grass, pokedexnum: 001},
+{   name: Squirtle, type: water, pokedexnum: 007},
+{   name: Pikachu, type: electric, pokedexnum: 025},
+]
+
+//call from list
+console.log(pokeBalls)
